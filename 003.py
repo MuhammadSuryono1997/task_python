115e1c4e3c5957d6c7e0e8238557994711f9319a
MuhammadSuryono1997 <rumahcintailmu@gmail.com>
2020-06-09 16:45:10 +0700

    Soal nomor 3

---
+++ 003.py
@@ -0,0 +1,12 @@
+a = int(input("Masukkan angka : "));
+
+if(a%2 == 0):
+  if(a > 20):
+    print("Crucio")
+  else:
+    print("Crucio")
+else:
+  if(a in range(5,10)):
+    print("Imperio")
+  else:
+    print("Imperio")
