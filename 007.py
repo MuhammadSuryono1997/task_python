83b69ef8e18b5d18f16fec702f01cf9d895071d5
MuhammadSuryono1997 <rumahcintailmu@gmail.com>
2020-06-09 21:30:24 +0700

    Soal nomor 7

---
+++ 007.py
@@ -0,0 +1,21 @@
+score = eval(input("your value = "))
+
+if score >= 90 :
+
+  print("grade A")
+
+elif score  >= 80 and score < 90 :
+
+  print("grade B")
+
+elif score >= 70 and score < 80 :
+
+  print("grade C")
+
+elif score  >= 60 and score < 70 :
+
+  print("grade D")
+
+elif score < 60 :
+
+  print("grade E")
