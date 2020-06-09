f1445670041794b55c50f0d6d4a625913b93b483
MuhammadSuryono1997 <rumahcintailmu@gmail.com>
2020-06-09 21:42:14 +0700

    Soal nomor 9

---
+++ 009.py
@@ -0,0 +1,10 @@
+usia = int(input("Masukkan usia : "))
+
+if usia >= 21:
+  print("DEWASA")
+elif usia >= 13:
+  print("REMAJA")
+elif usia >= 9:
+  print("BIMBINGAN ORANGTUA")
+elif usia < 9:
+  print("SEMUA USIA")
