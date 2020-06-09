68631edea79c2d2d43ab715a228fd4957d294500
MuhammadSuryono1997 <rumahcintailmu@gmail.com>
2020-06-09 16:26:03 +0700

    Soal nomor 1

---
+++ 001.py
@@ -0,0 +1,18 @@
+a = int(input("Inputkan nilai a: "))
+b = int(input("Inputkan nilai b: "))
+
+# Penjumlahan
+c = a + b
+print("Hasil %d + %d = %d" % (a,b,c))
+
+# Pengurangan
+c = a - b
+print("Hasil %d - %d = %d" % (a,b,c))
+
+# Pembagian
+c = a / b
+print("Hasil %d / %d = %d" % (a,b,c))
+
+# perpangkatan
+c = a ** 2
+print("Hasil %d ** 2 = %d" % (a,c))
